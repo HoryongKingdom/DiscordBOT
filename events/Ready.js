461717762e3b96b0a08829c5a6f5ddf91710e5f1
renu357 <gojune.g@gmail.com>
2023-08-17 20:53:05 +0900

    First

---
+++ events/Ready.js
@@ -0,0 +1,18 @@
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+  name: "ready",
+  once: true,
+  /**
+   *
+   * @param {import('discord.js').Client} client
+   */
+  async execute(client) {
+    console.log(`${client.user.tag} 로그인`);
+
+    client.user.setActivity({
+      name: `'/도움말'로 봇 사용`,
+      type: ActivityType.Playing,
+    });
+  },
+};
