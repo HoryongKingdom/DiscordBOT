c4aeb4865610ab2bb198c1868a8e672de75f63a7
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-08-20 20:36:32 +0900

    [ #1  ] dokdok 추가

---
+++ commands/admin/admin.js
@@ -0,0 +1,74 @@
+const {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('관리자')
+		.setDescription('관리자 기능을 사용합니다!')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ViewGuildInsights)
+		.addStringOption((option) =>
+			option
+				.setName('기능')
+				.setDescription('사용할 기능을 선택해주세요!')
+				.setRequired(true)
+				.addChoices({ name: '콘솔', value: 'console' }),
+		),
+	/**
+	 *
+	 * @param {import('discord.js').CommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		const checkmark = interaction.client.emojis.cache.get(
+			'1142775488054562948',
+		);
+		const cross = interaction.client.emojis.cache.get('1142775482132217868');
+		const user = interaction.user;
+		const category = interaction.options.getString('기능');
+		if (category === 'console') {
+			const embed = new EmbedBuilder()
+				.setTitle('💠 관리자 기능 사용하기')
+				.setDescription(
+					'**젠디**의 관리자용 기능을 여기서 간편하게 사용할 수 있어요!',
+				)
+				.setColor('#4f5fab')
+				.setFooter({
+					text: user.displayName,
+					iconURL: user.displayAvatarURL({ dynamic: true }),
+				})
+				.setTimestamp()
+				.addFields({
+					name: '✅ 인증 생성',
+					value: `>>> 인증을 생성하고, 사용합니다!`,
+					inline: true,
+				});
+			const button = new ActionRowBuilder().addComponents(
+				new ButtonBuilder()
+					.setLabel('인증 생성')
+					.setEmoji('✅')
+					.setCustomId('btn-dev-ver')
+					.setStyle(ButtonStyle.Secondary),
+			);
+			try {
+				await interaction.reply({
+					content: `${ checkmark }ㅣ**성공적으로 콘솔을 채널에 출력했어요!**`,
+					ephemeral: true,
+				});
+				await interaction.channel.send({
+					embeds: [ embed ],
+					components: [ button ],
+				});
+			} catch (err) {
+				await interaction.reply({
+					content: `${ cross }ㅣ**콘솔을 출력하는 동안 오류가 발생했어요! [ERROR 'i-1']**`,
+					ephemeral: true,
+				});
+			}
+		}
+	},
+};
--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
+const Dokdo = require('dokdo');
 dotenv.config();
 
-const { Client, Collection, REST, Routes } = require("discord.js");
-const client = (module.exports = new Client({ intents: [131071] }));
+const { Client, Collection, REST, Routes } = require('discord.js');
+const client = (module.exports = new Client({ intents: [ 131071 ] }));
 module.exports = client;
-client.login(process.env["TOKEN"]);
+client.login(process.env['TOKEN']);
 
-const fs = require("fs");
+const fs = require('fs');
 
-const eventsPath = "./events";
+const DokdoHandler = new Dokdo.Client(client, {
+	aliases: [ 'dokdo', 'debug', 'dok' ],
+	prefix: '!',
+});
+
+client.on('messageCreate', async (message) => {
+	await DokdoHandler.run(message);
+});
+
+const eventsPath = './events';
 const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
+	.readdirSync(eventsPath)
+	.filter((file) => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-  const filePath = `./${eventsPath}/${file}`;
-  const event = require(filePath);
-  if (event.once == true) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
+	const filePath = `./${ eventsPath }/${ file }`;
+	const event = require(filePath);
+	if (event.once == true) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 client.commands = new Collection();
 
 const commands_json = [];
 
-const commandsCategoryPath = "./commands";
+const commandsCategoryPath = './commands';
 const commandsCategoryFiles = fs.readdirSync(commandsCategoryPath);
 
 for (const category of commandsCategoryFiles) {
-  const commandsPath = `./commands/${category}`;
-  const commandsFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandsFiles) {
-    const command = require(`./commands/${category}/${file}`);
-    client.commands.set(command.data.name, command);
-    commands_json.push(command.data.toJSON());
-  }
+	const commandsPath = `./commands/${ category }`;
+	const commandsFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'));
+	for (const file of commandsFiles) {
+		const command = require(`./commands/${ category }/${ file }`);
+		client.commands.set(command.data.name, command);
+		commands_json.push(command.data.toJSON());
+	}
 }
 
-const rest = new REST({ version: "10" }).setToken(process.env["TOKEN"]);
+const rest = new REST({ version: '10' }).setToken(process.env['TOKEN']);
 
 rest
-  .put(Routes.applicationCommands(process.env.ID), { body: commands_json })
-  .then((command) => console.log(`${command.length}개의 커맨드를 푸쉬했습니다`))
-  .catch(console.error);
+	.put(Routes.applicationCommands(process.env.ID), { body: commands_json })
+	.then((command) => console.log(`${ command.length }개의 커맨드를 푸쉬했습니다`))
+	.catch(console.error);
