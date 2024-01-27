475d10a84d4c4791fe44938c005940a133640c37
레뉴 <gojune.g@gmail.com>
2024-01-27 15:48:12 +0900

    캡챠 오류 해결

--- events/BtnSelect.js
+++ events/BtnSelect.js
@@ -31,26 +31,26 @@ module.exports = {
 				})
 				.setTimestamp();
 			if (roleid === undefined) return await interaction.reply({ embeds: [ error ], ephemeral: true });
+			const captcha = new Captcha(client, {
+				roleID: roleid,
+				sendToTextChannel: false,
+				addRoleOnSuccess: true,
+				kickOnFailure: false,
+				caseSensitive: true,
+				attempts: 3,
+				timeout: 30000,
+				showAttemptCount: true,
+				customPromptEmbed: new EmbedBuilder()
+					.setTitle('✅ㅣ인증을 시작합니다!').setDescription('아래 영어 + 숫자를 이 채널의 DM에 대소문자를 구분하여 입력해주세요!').setColor('Green').setTimestamp(),
+				customSuccessEmbed: new EmbedBuilder()
+					.setTitle('✅ㅣ인증 완료!').setDescription(`인증이 완료되었습니다! 인증 역할이 지급되었어요!`).setColor('Green').setTimestamp(),
+				customFailureEmbed: new EmbedBuilder()
+					.setTitle('❌ㅣ인증에 실패했어요!').setDescription('캡챠 인증에 실패했어요! 다시 시도해주세요!').setColor('Red').setTimestamp(),
+			});
 			try {
-				const captcha = new Captcha(client, {
-					roleID: roleid,
-					sendToTextChannel: false,
-					addRoleOnSuccess: true,
-					kickOnFailure: false,
-					caseSensitive: true,
-					attempts: 3,
-					timeout: 30000,
-					showAttemptCount: true,
-					customPromptEmbed: new EmbedBuilder()
-						.setTitle('✅ㅣ인증을 시작합니다!').setDescription('아래 영어 + 숫자를 이 채널의 DM에 대소문자를 구분하여 입력해주세요!').setColor('Green').setTimestamp(),
-					customSuccessEmbed: new EmbedBuilder()
-						.setTitle('✅ㅣ인증 완료!').setDescription(`인증이 완료되었습니다! 인증 역할이 지급되었어요!`).setColor('Green').setTimestamp(),
-					customFailureEmbed: new EmbedBuilder()
-						.setTitle('❌ㅣ인증에 실패했어요!').setDescription('캡챠 인증에 실패했어요! 다시 시도해주세요!').setColor('Red').setTimestamp(),
-				});
-				
+				await interaction.deferReply({ ephemeral: true });
 				await captcha.present(interaction.member);
-				await interaction.reply({ content: '### ✅ㅣDM으로 인증 메시지를 전송해드렸어요!', ephemeral: true });
+				await interaction.editReply({ content: '### ✅ㅣDM으로 인증 메시지를 전송해드렸어요!' });
 			} catch (err) {
 				console.error(err);
 			}
